7f861615c49806683dd934d32388139f20483d37
Macy Ngan <macy@Macys-MacBook-Air.local>
2017-12-15 02:56:08 -0800

    upload projects

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('loadPortfolio', function($scope, $http, $interval){
+
+
+});
+
+/*
+$('#webpagespeedModalCard').click(function() {
+  $('#webpagespeedModal').modal('show')
+});
+*/
